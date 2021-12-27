7d431f7c7b3851accbed880d090bd0d260fdf484
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2021-12-27 19:24:59 +0700

    Update Exercise

--- ex00/libft_creator.sh
+++ ex00/libft_creator.sh
@@ -3,21 +3,3 @@ find . -maxdepth 1 -name "*.c" -type f -exec gcc -Wall -Werror -Wextra -c {} \;
 ar -rcs libft.a *.o
 find . -maxdepth 1 -name "*.o" -type f -delete
 
-echo "===== norminette -v ========="
-norminette -v
-echo "===== lsb_release -a ========="
-lsb_release -a
-echo "===== xcode-select -p ========="
-lsb_release -a
-echo "===== uname -a ========="
-uname -a
-echo "===== uname -m ========="
-uname -m
-echo "===== hostnamectl ========="
-hostnamectl
-echo "===== gcc -v ========="
-gcc -v
-echo "===== clang -v ========="
-clang -v
-echo "===== printenv ========="
-printenv
